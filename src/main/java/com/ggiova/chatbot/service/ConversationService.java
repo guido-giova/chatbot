95b05bfecf8ecad715fb99c84a1c1c36c47aee0d
ggiovadam <guido.giovadam@gmail.com>
2026-09-08 17:11:08 +0200

    [feat] Add list conversations to ConversationService

--- src/main/java/com/ggiova/chatbot/service/ConversationService.java
+++ src/main/java/com/ggiova/chatbot/service/ConversationService.java
@@ -1,4 +1,18 @@
 package com.ggiova.chatbot.service;
 
+import com.ggiova.chatbot.entity.Conversation;
+import com.ggiova.chatbot.repository.ConversationRepository;
+
+import java.util.List;
+
 public class ConversationService {
+    private final ConversationRepository conversationRepository;
+    
+    ConversationService(ConversationRepository conversationRepository) {
+        this.conversationRepository = conversationRepository;
+    }
+    
+    public List<Conversation> listConversations() {
+        return this.conversationRepository.findAll();
+    }
 }
