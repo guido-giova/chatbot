251601fbfbc43dda0cb9a08fd840beb3c97036d0
ggiovadam <guido.giovadam@gmail.com>
2026-09-08 17:55:43 +0200

    [chore] Add Javadoc to ConversationMapper

--- src/main/java/com/ggiova/chatbot/mapper/ConversationMapper.java
+++ src/main/java/com/ggiova/chatbot/mapper/ConversationMapper.java
@@ -4,6 +4,9 @@ import com.ggiova.chatbot.dto.conversation.ConversationListingDTO;
 import com.ggiova.chatbot.entity.Conversation;
 import org.mapstruct.Mapper;
 
+/**
+ * Mapper for the Conversations
+ */
 @Mapper(componentModel = "spring")
 public interface ConversationMapper
          extends MapperParent<Conversation, ConversationListingDTO> {
