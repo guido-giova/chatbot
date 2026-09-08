a680e153f6e46ed08722ded2f4ec3466700dc427
ggiovadam <guido.giovadam@gmail.com>
2026-09-08 17:52:47 +0200

    [chore] Add Javadoc to MapperParent and mapper package

--- src/main/java/com/ggiova/chatbot/mapper/MapperParent.java
+++ src/main/java/com/ggiova/chatbot/mapper/MapperParent.java
@@ -4,10 +4,36 @@ import com.ggiova.chatbot.dto.ListWrapper;
 
 import java.util.List;
 
+/**
+ * Maps from the entity to the DTO
+ *
+ * @param <E> Entity (source)
+ * @param <D> DTO (Target)
+ */
 public interface MapperParent<E, D> {
+    /**
+     * Maps the given entity into the DTO
+     *
+     * @param entity that will be mapped
+     * @return the DTO that represents the entity
+     */
     D toDto(E entity);
+    
+    /**
+     * Maps each one of the given entities into their DTOs
+     *
+     * @param entities that will be mapped
+     * @return A list containing DTOs
+     */
     List<D> toDtoList(List<E> entities);
     
+    /**
+     * Maps the given entities into their DTOs and then wraps them in ListWrapper
+     *
+     * @param entities that will be mapped
+     * @return a wrapper containing the mapped DTOs
+     * @see ListWrapper
+     */
     default ListWrapper<D> toListWrapper(List<E> entities) {
         return new ListWrapper<>(this.toDtoList(entities));
     }
---
+++ src/main/java/com/ggiova/chatbot/mapper/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package containing the mappers for the entities
+ *
+ * @see com.ggiova.chatbot.mapper.MapperParent
+ * @see com.ggiova.chatbot.mapper.ConversationMapper
+ */
+package com.ggiova.chatbot.mapper;
