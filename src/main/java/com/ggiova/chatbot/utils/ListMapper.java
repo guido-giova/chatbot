32c0930c9e6051542958636f56cd304b11081962
ggiovadam <guido.giovadam@gmail.com>
2026-09-08 16:41:17 +0200

    [feat] Add a method to map the objects before creating the ListWrapper

--- src/main/java/com/ggiova/chatbot/utils/ListMapper.java
+++ src/main/java/com/ggiova/chatbot/utils/ListMapper.java
@@ -1,11 +1,16 @@
 package com.ggiova.chatbot.utils;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class ListMapper {
     public record ListWrapper<T>(List<T> data) {}
     
     public static <T> ListWrapper<T> toListWrapper(List<T> toWrap) {
         return new ListWrapper<T>(toWrap);
+    }
+    
+    public static <T, R> ListWrapper<R> toListWrapper(List<T> toWrap, Function<T, R> mapper) {
+        return new ListWrapper<>(toWrap.stream().map(mapper).toList());
     }
 }
