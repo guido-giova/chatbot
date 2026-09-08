6b28ded2bac4700e3d3086cd7b120045c3ecd0fe
ggiovadam <guido.giovadam@gmail.com>
2026-09-08 16:27:53 +0200

    [chore] Add Javadoc to Config

--- src/main/java/com/ggiova/chatbot/config/Config.java
+++ src/main/java/com/ggiova/chatbot/config/Config.java
@@ -4,8 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tools.jackson.databind.ObjectMapper;
 
+/**
+ * Configuration class for the app.
+ * <p>
+ * Currently has a bean for ObjectMapper.
+ */
 @Configuration
 public class Config {
+    /**
+     * @return a new ObjectMapper
+     * @see ObjectMapper
+     */
     @Bean
     ObjectMapper objectMapper() {
         return new ObjectMapper();
